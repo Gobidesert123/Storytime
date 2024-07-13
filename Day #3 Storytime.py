c35494a1d84db39640cc5e754771942e0fa3a6d8
Gobind Kailey <160374905+Gobidesert123@users.noreply.github.com>
2024-07-13 16:20:37 -0400

    Fixing errors with code
    For some reason I used an elif statement with a y variable. I have updated the elif statement to be elif choice_1.

--- Day #3 Storytime.py
+++ Day #3 Storytime.py
@@ -20,13 +20,13 @@ print('''
 
 print("Welcome to the Jungle")
 # We added .lower to the end, just incase the user inputed an uppercase letter
-choice_1 = input("Would you like to go left to right?").lower()
-print(choice_1)
+choice_1 = input("Would you like to go left to right? ").lower()
+# print(choice_1)
 if choice_1 == "left":
-    choice_2 = input("Would you like to run or wait?").lower()
-    print(choice_2)
+    choice_2 = input("Would you like to run or wait? ").lower()
+    # print(choice_2)
     if choice_2 == "run":
-        choice_3 = input("Which door would you choose to hide in? blue, red or yellow").lower()
+        choice_3 = input("Which door would you choose to hide in? blue, red or yellow ").lower()
         if choice_3 == "blue":
             print("You died")
         elif choice_3 == "red":
@@ -39,9 +39,8 @@ if choice_1 == "left":
         print("You have been eaten alive")
     else:
         print("You have given an invalid output")
-elif y == "right":
+elif choice_1 == "right":
     print("You have been eaten alive")
 else:
     print("You have given an invalid output")
-print("new change")
 
